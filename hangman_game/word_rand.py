39fa1b09b34df9143bd81e1faa5c4cc38c7a59eb
Shamiul Alam <1hourprograms@gmail.com>
2022-08-23 22:29:09 +0600

    Fixed some issue in Hangman Game.

--- hangman_game/word_rand.py
+++ hangman_game/word_rand.py
@@ -3,8 +3,7 @@ import random
 def randWord():
     words = ['definition', 'context', 'sir', 'unit', 'candidate']
 
-    for i in range(len(words)):
-        word = words[random.randint(0, i)]
+    word = words[random.randint(0, len(words)-1)]
 
     print(f'The length of the word is {len(word)}')
 
